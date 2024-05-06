ca0e280ddeba54148b667655b82345eb0c2b5c1c
daquinoo <95264544+daquinoo@users.noreply.github.com>
2024-05-05 19:54:24 -0700

    Create findnonexistingnums.java

---
+++ findnonexistingnums.java
@@ -0,0 +1,19 @@
+// Given an array nums of n integers where nums[i] is in the range [1, n], return an array of all the integers in the range [1, n] that do not appear in nums.
+
+class Solution {
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        List<Integer> result = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            int index = Math.abs(nums[i]) - 1; 
+            if (nums[index] > 0) {
+                nums[index] = -nums[index]; 
+            }
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > 0) {
+                result.add(i + 1);
+            }
+        }
+        return result;
+    }
+}
