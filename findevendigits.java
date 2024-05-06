46dd6f9581980df1021d9a77fd69a3aeda94b36b
daquinoo <95264544+daquinoo@users.noreply.github.com>
2024-05-05 18:18:23 -0700

    Create findevendigits.java

---
+++ findevendigits.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int findNumbers(int[] nums) {
+        int count = 0;
+        int digits = 0;
+        for (int i=0; i<nums.length; i++)
+        {
+            int num = nums[i];
+            digits = 0;
+            while (num >= 10)
+            {
+                num /= 10;
+                digits++;
+            }
+            if (num > 0)
+            {
+                digits++;
+            }
+            if (digits %2 == 0)
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+}
